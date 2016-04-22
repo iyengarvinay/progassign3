dd2ba7a04cc4e0319afaeffc4a9656cf66390223
James Baskerville <baskerville@college.harvard.edu>
2016-04-21 21:07:03 -0400

    check commit

--- kk.py
+++ kk.py
@@ -1,14 +1,19 @@
 import fileinput
+import bisect
 import sys
 
 if (len(sys.argv) != 2):
     print ("usage: python <inputfile>")
     exit(1)
 
-A = [line.rstrip('\n') for line in open(sys.argv[1])]
-
-print(A)
+A = [int(line.rstrip('\n')) for line in open(sys.argv[1])]
 
 A.sort()
 
-print(A)
+for i in range(len(A) - 1):
+    a1 = A.pop()
+    a2 = A.pop()
+    diff = a1 - a2
+    bisect.insort(A, diff)
+
+print(A[0])
