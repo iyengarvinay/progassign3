ba268c1c6625e058f3050f49d334d322d81d4662
James Baskerville <baskerville@college.harvard.edu>
2016-04-21 20:46:17 -0400

    sorted list

--- kk.py
+++ kk.py
@@ -1,8 +1,14 @@
 import fileinput
+import sys
 
 if (len(sys.argv) != 2):
     print ("usage: python <inputfile>")
     exit(1)
 
-for line in fileinput.input(sys.argv[1]):
-    #process(line)
+A = [line.rstrip('\n') for line in open(sys.argv[1])]
+
+print(A)
+
+A.sort()
+
+print(A)
