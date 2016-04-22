015bda78d983017a3b03a5f3b4c95637bf931f02
iyengarvinay <vinaysiyengar@gmail.com>
2016-04-21 22:37:06 -0400

    added random partition

--- runKK.py
+++ runKK.py
@@ -21,7 +21,7 @@ def kkRun(numList):
     return (A[0])
 
 
-def repeatedRandom(numList):
+def repeatedRandomSolution(numList):
     minResidue = max(numList)
     for j in range(25000):
         randSolution = [0]*100
@@ -34,12 +34,27 @@ def repeatedRandom(numList):
 
     return minResidue
 
-results = [["Random Set", "KK Result", "Repeated Random", "Hill Climbing", "Simulated Annealing"]]
+def repeatedRandomPartition(numList):
+    minResidue = max(numList)
+    for j in range(25000):
+        randPartition = [0]*100
+        numListModified = [0]*100
+        for i in range(100):
+            randPartition[i] = random.randint(0,99)
+            numListModified[randPartition[i]] = numList[i] + numListModified[randPartition[i]]
+        residue = kkRun(numListModified)
+        if(residue < minResidue):
+            minResidue = residue
+
+    return minResidue
+
+
+results = [["Random Set", "KK Result", "Repeated Random - Solution", "Repeated Random - Partition", "Hill Climbing - Solution", "Hill Climbing - Partition", "Simulated Annealing - Solution", "Simulated Annealing - Partition"]]
 
 for i in range(int(sys.argv[1])):
     filename = "nums" + str(i) + ".txt"
     A = [int(line.rstrip('\n')) for line in open(filename)]
-    results.append([i, kkRun(A), repeatedRandom(A)])
+    results.append([i, kkRun(A), repeatedRandomSolution(A), repeatedRandomPartition(A)])
 
 print (results)
 
