43428c4e716873f6db0cadb6ee0040efc2174bc9
James Baskerville <baskerville@college.harvard.edu>
2016-04-22 14:49:03 -0400

    code done; just debug

--- runKK.py
+++ runKK.py
@@ -25,28 +25,37 @@ def kkRun(numList, n):
 
     return (A[0])
 
-def repeatedRandomSolution(numList, n):
-    minResidue = max(numList)
+def createRandSol(numList, n):
+    randSolution = [0]*n
+    residue = 0
+    for i in range(n):
+        randSolution[i] = random.choice([-1,1])
+        residue = abs(residue + (randSolution[i] * numList[i]))
+    return (randSolution, residue)
+
+def createRandPart(numList, n):
+    randPartition = [0]*n
+    residue = 0
+    numListModified = [0]*n
+    for i in range(n):
+        randPartition[i] = random.randint(0,n-1)
+        numListModified[randPartition[i]] = numList[i] + numListModified[randPartition[i]]
+    residue = kkRun(numListModified, n)
+    return (randPartition, residue)
+
+def repeatedRandomSolution(res, numList, n):
+    minResidue = res
     for j in range(MAX_ITER):
-        randSolution = [0]*n
-        residue = 0
-        for i in range(n):
-            randSolution[i] = random.choice([-1,1])
-            residue = abs(residue + (randSolution[i] * numList[i]))
+        residue = (createRandSol(numList, n))[1]
         if(residue < minResidue):
             minResidue = residue
 
     return minResidue
 
-def repeatedRandomPartition(numList, n):
-    minResidue = max(numList)
+def repeatedRandomPartition(res, numList, n):
+    minResidue = res
     for j in range(MAX_ITER):
-        randPartition = [0]*n
-        numListModified = [0]*n
-        for i in range(n):
-            randPartition[i] = random.randint(0,n-1)
-            numListModified[randPartition[i]] = numList[i] + numListModified[randPartition[i]]
-        residue = kkRun(numListModified)
+        residue = (createRandPart(numList, n))[1]
         if(residue < minResidue):
             minResidue = residue
 
@@ -55,12 +64,9 @@ def repeatedRandomPartition(numList, n):
 # choose random solution in neighborhood
 # change one sign with probability 100 / 5050 (100 = 100 choose 1)
 # change two with probability 4950 / 5050 (4950 = 100 choose 2)
-def hillClimb(numList, n):
-    randSolution = [0]*n
-    residue = 0
-    for i in range(n):
-        randSolution[i] = random.choice([-1,1])
-        residue = abs(residue + (randSolution[i] * numList[i]))
+def hillClimb(S, res, numList, n):
+    randSolution = S
+    residue = res
     minResidue = residue
 
     # find random neighbor of S
@@ -92,51 +98,51 @@ def hillClimb(numList, n):
 
     return minResidue
 
-def hillClimbPartition(numList, n):
-    randSolution = [0]*n
-    residue = 0
-    for i in range(n):
-        randSolution[i] = random.choice([-1,1])
-        residue = abs(residue + (randSolution[i] * numList[i]))
+def hillClimbPartition(P, res, numList, n):
+
+    # create random partition & evaluate inital residue
+    randPartition = P
+    numListModified = [0]*n
+    residue = res
     minResidue = residue
 
-    # find random neighbor of S
     for j in range(MAX_ITER):
-        # copy solution list in order to change
-        neighbor = list(randSolution)
+        # copy partition
+        neighbor = list(randPartition)
 
-        # make one switch
-        switch = random.randint(0,n-1)
-        neighbor[switch] *= -1
+        # make first switch
+        switch = random.randint(0, n-1)
+        neighbor[switch] = random.randint(0, n-1)
+        while (neighbor[switch] == randPartition[switch]):
+            neighbor[switch] = random.randint(0, n-1)
 
         # second switch with prob 1/2
         if (random.getrandbits(1) == 1):
-            switch2 = random.randint(0,n-1)
+            switch2 = random.randint(0, n-1)
             while (switch == switch2):
-                switch2 = random.randint(0,n-1)
-            neighbor[switch2] *= -1
-        
-        #print("neighbor {}", j)
-        #printSol(numList, neighbor, n)
+                switch2 = random.randint(0, n-1)
+            neighbor[switch2] = random.randint(0, n-1)
+            while (neighbor[switch2] == randPartition[switch2]):
+                neighbor[switch2] = random.randint(0, n-1)
 
-        residue = 0
+        numListModified = [0]*n
         for i in range(n):
-            residue = abs(residue + (neighbor[i] * numList[i]))
+            #neighbor[i] = random.randint(0, n-1)
+            numListModified[neighbor[i]] = numList[i] + numListModified[neighbor[i]]
 
-        if(residue < minResidue):
+        residue = kkRun(numListModified, n)
+
+        if (residue < minResidue):
             minResidue = residue
-            randSolution = list(neighbor)
+            randPartition = list(neighbor)
 
     return minResidue
 
-def simAnneal(numList, n):
+def simAnneal(S, res, numList, n):
 
     # create random solution S
-    randSolution = [0]*n
-    residue = 0
-    for i in range(n):
-        randSolution[i] = random.choice([-1,1])
-        residue = abs(residue + (randSolution[i] * numList[i]))
+    randSolution = S
+    residue = res
     minResidue = residue
     homeSolution = list(randSolution)
     homeRes = minResidue
@@ -196,6 +202,62 @@ def simAnneal(numList, n):
     #print(succswitches)
     return minResidue
 
+def simAnnealPartition(P, res, numList, n):
+
+    # create random partition & evaluate inital residue
+    randPartition = P
+    numListModified = [0]*n
+    residue = res
+    minResidue = residue
+    homePartition = list(randPartition)
+    homeRes = minResidue
+
+    for j in range(MAX_ITER):
+        # copy partition
+        neighbor = list(randPartition)
+
+        # make first switch
+        switch = random.randint(0, n-1)
+        neighbor[switch] = random.randint(0, n-1)
+        while (neighbor[switch] == randPartition[switch]):
+            neighbor[switch] = random.randint(0, n-1)
+
+        # second switch with prob 1/2
+        if (random.getrandbits(1) == 1):
+            switch2 = random.randint(0, n-1)
+            while (switch == switch2):
+                switch2 = random.randint(0, n-1)
+            neighbor[switch2] = random.randint(0, n-1)
+            while (neighbor[switch2] == randPartition[switch2]):
+                neighbor[switch2] = random.randint(0, n-1)
+
+        numListModified = [0]*n
+        for i in range(n):
+            numListModified[neighbor[i]] = numList[i] + numListModified[neighbor[i]]
+
+        residue = kkRun(numListModified, n)
+
+        if (residue <= minResidue):
+            minResidue = residue
+            randPartition = list(neighbor)
+
+        else:
+            tens = pow(10,10)
+            bottom = pow(0.8,(math.floor(j/300)))
+            resDiff = -(residue - minResidue)
+            exponent = resDiff/(tens*bottom)
+            #print(exponent)
+            prob = math.exp(exponent)
+            if (random.uniform(0,1) <= prob):
+                randPartition = list(neighbor)
+                minResidue = residue
+
+        if (residue < homeRes):
+            homePartition = list(neighbor)
+            homeRes = residue
+
+    return minResidue
+
 def printSol(numList, signList, n):
     A = []
     B = []
@@ -213,14 +275,16 @@ lenTest = len(test)
 
 # compute results
 #results = [["Random Set", "KK Result", "Repeated Random - Solution", "Repeated Random - Partition", "Hill Climbing - Solution", "Hill Climbing - Partition", "Simulated Annealing - Solution", "Simulated Annealing - Partition"]]
-results = [["Random Set", "KK", "RR", "Hill Climb", "Simulated Annealing"]]
+results = [["Random Set", "KK", "RR", "RR Partition", "Hill Climb", "Hill Climb Partition", "Simulated Annealing", "SimAnneal Partition"]]
 for i in range(int(sys.argv[1])):
     filename = "nums" + str(i) + ".txt"
     A = [int(line.rstrip('\n')) for line in open(filename)]
     lenA = len(A)
+    initS = createRandSol(A, lenA)
+    initP = createRandPart(A, lenA)
     #results.append([i, kkRun(A), repeatedRandomSolution(A), repeatedRandomPartition(A), hillClimb(A), simAnneal(A)])
     #results.append([i, kkRun(test, lenTest), repeatedRandomSolution(test, lenTest), hillClimb(test, lenTest), simAnneal(test, lenTest)])
-    results.append([i, kkRun(A, lenA), repeatedRandomSolution(A, lenA), hillClimb(A, lenA), simAnneal(A, lenA)])
+    results.append([i, kkRun(A, lenA), repeatedRandomSolution(initS[1], A, lenA), repeatedRandomPartition(initP[1], A, lenA), hillClimb(initS[0], initS[1], A, lenA), hillClimbPartition(initP[0], initP[1], A, lenA), simAnneal(initS[0], initS[1], A, lenA), simAnnealPartition(initP[0], initP[1], A, lenA)])
 for line in results:
     print (line)
 
